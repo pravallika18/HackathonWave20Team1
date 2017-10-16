7423c90792b733273bc59650013927f844395e49
admin <ashishgupta@ibigrock.com>
2017-10-16 10:52:09 +0530

    User service pacKAGE updated

--- src/main/java/com/wipro/SpringRest/service/UserService.java
+++ src/main/java/com/wipro/SpringRest/service/UserService.java
@@ -1,5 +1,19 @@
 package com.wipro.SpringRest.service;
 
-public interface UserService {
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.wipro.SpringRest.domain.User;
+@Service
+public interface UserService  //User Interface to declare the oprating abstract methods
+{
+	public User createProfile(User user);
+	public String updateProfile(User user);
+	public String deleteProfile(User user);
+	public List<User> getAll();
+	public User getById(User user);
+	
+	
 
 }
